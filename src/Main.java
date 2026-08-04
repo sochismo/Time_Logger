bb8af2ab0af0f822d3959b28341150f7a1615091
sochismo <sophia.jost1607@gmail.com>
2026-08-04 19:55:56 +0200

    Day 1: variables and project name print loop :P

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String projectName = "The epic time logger";
+        byte startTime = 0;
+        byte endTime = 60;
+        System.out.println(projectName);
+    }
+}
