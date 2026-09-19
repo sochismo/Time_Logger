6cbe0e84343cca835237aaa1274dac997b10a301
sochismo <sophia.jost1607@gmail.com>
2026-09-20 01:12:08 +0200

    Day 2: Operators and such, just a tiny update to get back on track :D

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String projectName = "The epic time logger";
-        byte startTime = 0;
-        byte endTime = 60;
+        byte startTime = 0; //Currently a placeholder, sooner or later it can be any number
+        byte endTime = 60; //Same thing here
         System.out.println(projectName);
+        int duration = endTime - startTime; //This variable calculates the difference of your start and end time in mins
+        System.out.println(duration);
     }
 }
